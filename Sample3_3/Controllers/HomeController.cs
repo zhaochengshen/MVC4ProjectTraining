571fad9fb7ff35f0c922d13b8d444191245883a4
zhaochengshen <zhaochengshen@163.com>
2016-05-30 13:19:47 +0800

    Add Project

---
+++ Sample3_3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample3_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>();
+            list.Add("Red");
+            list.Add("blue");
+            list.Add("black");
+            list.Add("green");
+            list.Add("grey");
+            list.Add("orange");
+
+            this.ViewBag.ViewBagListColors = list;
+            this.ViewData["ViewDataListColors"] = list;
+
+            return View();
+        }
+
+    }
+}
